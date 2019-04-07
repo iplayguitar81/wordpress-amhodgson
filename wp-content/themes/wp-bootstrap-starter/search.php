881e4ee15223f2122a9b549dbd9cedb14a8dd672
iplayguitar81 <iplayguitar81@yahoo.com>
2019-04-07 11:07:49 -0700

    closer to finishing book page template :)

--- wp-content/themes/wp-bootstrap-starter/search.php
+++ wp-content/themes/wp-bootstrap-starter/search.php
@@ -9,40 +9,51 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area col-sm-12 col-lg-8">
-		<main id="main" class="site-main" role="main">
+    <div class="container">
+    <br/>
 
 		<?php
 		if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'wp-bootstrap-starter' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+				<h1 class="page-title georgia"><i class="fas fa-search"></i> <?php printf( esc_html__( 'Search Results for: %s', 'wp-bootstrap-starter' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 			</header><!-- .page-header -->
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+            <?php
+            /* Start the Loop */
+            while ( have_posts() ) : the_post();
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+                /*
+                 * Include the Post-Format-specific template for the content.
+                 * If you want to override this in a child theme, then include a file
+                 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                 */
+                get_template_part( 'template-parts/content', get_post_format() );
 
-			endwhile;
+            endwhile;
 
-			the_posts_navigation();
+            echo '<div class="pagination mx-auto">';
+            the_posts_pagination();
+            echo '</div><br/>';
 
-		else :
+        else :
 
-			get_template_part( 'template-parts/content', 'none' );
+            get_template_part( 'template-parts/content', 'none' );
 
-		endif; ?>
+        endif; ?>
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+    </div>
+
+
+    <div class="row georgia mt-3">
+        <br/>
+        <?php
+        get_sidebar();
+        ?>
+    </div>
+
+
 
 <?php
-get_sidebar();
+
 get_footer();
