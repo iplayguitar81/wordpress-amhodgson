7fa0f9ed1bf9e64d1603330b49974cd27702328b
iplayguitar81 <iplayguitar81@yahoo.com>
2019-04-06 22:11:27 -0700

    closer to finishing book page template :)

--- wp-content/themes/wp-bootstrap-starter/author.php
+++ wp-content/themes/wp-bootstrap-starter/author.php
@@ -43,12 +43,13 @@ if ( has_post_thumbnail() ) {
 
     echo '<div class="col-lg-4 col-md-4 col-sm-12 col-12 order-1 order-lg-1 order-sm-1 mx-auto">';
     the_post_thumbnail('original', array('class' => 'img-fluid img-thumbnail'));
-    echo '</div>';
+    echo '</div><div class="col-lg-8 col-md-8 col-sm-12 col-12 order-1 order-lg-1 order-sm-1 mx-auto mt-4">';
 
 }
 
 else {
 
+    echo '<div class="col-lg-12 col-md-12 col-sm-12 col-12 order-1 order-lg-1 order-sm-1 mx-auto mt-4">';
 
 }
 
@@ -60,7 +61,7 @@ else {
 <!--    --><?php //the_post_thumbnail('original', array('class' => 'img-fluid img-thumbnail'));?>
 <!--    </div>-->
 
-    <div class="col-lg-8 col-md-8 col-sm-12 col-12 order-1 order-lg-1 order-sm-1 mx-auto mt-4">
+<!--    <div class="col-lg-8 col-md-8 col-sm-12 col-12 order-1 order-lg-1 order-sm-1 mx-auto mt-4">-->
     <h2 class="chand mt-2 blog-index-titles"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
         <div class="georgia"><p class="author-excerpts">
