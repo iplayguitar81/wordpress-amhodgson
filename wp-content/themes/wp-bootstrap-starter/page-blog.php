259c4e8a72a69426684296f35678b3382120beaf
iplayguitar81 <iplayguitar81@yahoo.com>
2019-04-07 19:04:58 -0700

    closer to finishing book page template :)

--- wp-content/themes/wp-bootstrap-starter/page-blog.php
+++ wp-content/themes/wp-bootstrap-starter/page-blog.php
@@ -1,40 +1,181 @@
 <?php
 /**
- * The template for displaying all pages
+ * The main template file
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package WP_Bootstrap_Starter
  */
 
-get_header(); ?>
-    <br/>
-    <br/>
+get_header();
 
-    <section id="primary" class="content-area col-sm-12 col-lg-8">
-        <main id="main" class="site-main" role="main">
+?>
 
-            <?php
-            while ( have_posts() ) : the_post();
+    <div class="container">
 
-                get_template_part( 'template-parts/content', 'page' );
+    <div class="col-lg-12 col-md-12 col-sm-12 col-12 mx-auto mt-5">
+        <?php
+        while ( have_posts() ) : the_post();
 
-                // If comments are open or we have at least one comment, load up the comment template.
-                if ( comments_open() || get_comments_number() ) :
-                    comments_template();
-                endif;
+            the_title( '<h1 class="text-center entry-title chand">', '</h1>' );
 
-            endwhile; // End of the loop.
-            ?>
+            echo'<div class="entry-content ec-author georgia mx-auto">';
+            the_content();
 
-        </main><!-- #main -->
-    </section><!-- #primary -->
+            echo '</div>';
+
+            // If comments are open or we have at least one comment, load up the comment template.
+            if ( comments_open() || get_comments_number() ) :
+                comments_template();
+            endif;
+
+        endwhile; // End of the loop.
+
+        edit_post_link(
+            sprintf(
+            /* translators: %s: Name of current post */
+                esc_html__( 'Edit %s', 'wp-bootstrap-starter' ),
+                the_title( '<span class="screen-reader-text">"', '"</span>', false )
+            ),
+            '<span class="edit-link">',
+            '</span>'
+        );
+        ?>
+
+        <hr/>
+
+    </div>
 
 <?php
-//get_sidebar();
+
+$preview_posts = query_posts( array(
+    'posts_per_page' => -1,
+    'orderby' => 'date',
+    'posts_per_page' => 5,
+    'paged' => ( get_query_var('paged') ? get_query_var('paged') : 1),
+));
+
+foreach ( $preview_posts as $post ) :
+    setup_postdata( $post ); ?>
+    <div class="row mt-4">
+
+
+        <?php
+
+        if ( has_post_thumbnail() ) {
+
+            echo '<div class="col-lg-4 col-md-4 col-sm-12 col-12 order-1 order-lg-2 order-md-2 order-sm-1 mx-auto">';
+
+            echo '<a href="';
+            the_permalink();
+            echo '">';
+            the_post_thumbnail('original', array('class' => 'img-fluid img-thumbnail'));
+            echo '</a>';
+            echo '</div><div class="col-lg-8 col-md-8 col-sm-12 col-12 order-2 order-lg-1 order-md-1 order-sm-2 mx-auto mt-4">';
+
+            //echo '<h2 class="chand mt-2 blog-index-titles">';
+
+            echo '<h2 class="blog-index-titles chand mt-3"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">';
+
+            echo ucwords(get_the_title());
+
+            echo '</a></h2>';
+
+        }
+
+        else {
+
+            echo '<div class="col-lg-12 col-md-12 col-sm-12 col-12 order-1 order-lg-1 order-sm-1 mx-auto mt-4">';
+
+            echo '<h2 class="blog-index-titles chand mt-3 text-center"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">';
+
+            echo ucwords(get_the_title());
+
+            echo '</a></h2>';
+
+        }
+
+        if ( has_post_thumbnail() ) {
+            echo '<div class="entry-meta">';
+
+        }
+
+        else {
+            echo '<div class="entry-meta text-center">';
+
+        }
+
+        wp_bootstrap_starter_posted_on();
+
+
+        ?>
+
+
+
+    </div>
+    <br/>
+
+
+
+    <div class="georgia"><p class="author-excerpts">
+            <?php echo wp_trim_words( get_the_excerpt(), 35, '...  ' ); ?><a href="<?php the_permalink()?>" class="btn btn-md btn-dark"> Read More</a></p>
+    </div>
+    <br/>
+    <?php
+    edit_post_link(
+        sprintf(
+        /* translators: %s: Name of current post */
+            esc_html__( 'Edit %s', 'wp-bootstrap-starter' ),
+            the_title( '<span class="screen-reader-text">"', '"</span>', false )
+        ),
+        '<span class="edit-link">',
+        '</span>'
+    );
+    ?>
+
+
+
+    </div>
+
+
+    </div>
+    <br/>
+    <hr/>
+
+<?php endforeach;
+
+wp_reset_postdata(); ?>
+
+    <div class="pagination mx-auto"><?php the_posts_pagination(); ?></div>
+
+
+
+    <br/>
+
+    </div>
+
+    <br/>
+
+    <div class="row georgia mt-3">
+
+
+        <?php
+
+        get_sidebar();
+        ?>
+
+
+    </div>
+
+
+
+
+
+<?php
+
 get_footer();
