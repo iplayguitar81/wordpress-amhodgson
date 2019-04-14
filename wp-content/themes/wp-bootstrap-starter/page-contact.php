fac342368f7fffbd4bf42c65d3e9fa5df5aa054a
iplayguitar81 <iplayguitar81@yahoo.com>
2019-04-13 17:00:55 -0700

    closer to finishing book page template :)

--- wp-content/themes/wp-bootstrap-starter/page-contact.php
+++ wp-content/themes/wp-bootstrap-starter/page-contact.php
@@ -22,15 +22,15 @@ function my_contact_form_generate_response($type, $message){
 
     global $response;
 
-    if($type == "success") $response = "<div class='success'>{$message}</div>";
-    else $response = "<div class='error'>{$message}</div>";
+    if($type == "success") $response = "<div class='success georgia bg-success text-dark'><i class='fas fa-thumbs-up'></i> {$message}</div>";
+    else $response = "<div class='error georgia bg-secondary text-white'><i class='fas fa-exclamation-triangle'></i> {$message}</div>";
 
 }
 
 
 //response messages
 $not_human       = "Human verification incorrect.";
-$missing_content = "Please supply all information.";
+$missing_content = "Please be sure to fill all fields out!";
 $email_invalid   = "Email Address Invalid.";
 $message_unsent  = "Message was not sent. Try Again.";
 $message_sent    = "Thanks! Your message has been sent.";
@@ -46,6 +46,32 @@ $to = get_option('admin_email');
 $subject = "Someone sent a message from ".get_bloginfo('name');
 $headers = 'From: '. $email . "\r\n" .
     'Reply-To: ' . $email . "\r\n";
+
+if(!$human == 0){
+    if($human != 2) my_contact_form_generate_response("error", $not_human); //not human!
+    else {
+
+        //validate email
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+            my_contact_form_generate_response("error", $email_invalid);
+        else //email is valid
+        {
+            //validate presence of name and message
+            if(empty($name) || empty($message)){
+                my_contact_form_generate_response("error", $missing_content);
+            }
+            else //ready to go!
+            {
+                //send email
+                $sent = wp_mail($to, $subject, strip_tags($message), $headers);
+                if($sent) my_contact_form_generate_response("success", $message_sent); //message sent!
+                else my_contact_form_generate_response("error", $message_unsent); //message wasn't sent
+            }
+        }
+    }
+}
+else if ($_POST['submitted']) my_contact_form_generate_response("error", $missing_content);
+
 
 
 get_header(); ?>
@@ -76,7 +102,9 @@ get_header(); ?>
             endwhile; // End of the loop.
             ?>
 
+
                 <?php echo $response; ?>
+                <br/>
                 <form class="georgia" action="<?php the_permalink(); ?>" method="post">
                     <div class="form-group"><label class="col-form-label" for="name">Name</label><input class="form-control form-control-lg" type="text" name="message_name" value="<?php echo esc_attr($_POST['message_name']); ?>"></div>
                     <div class="form-group"><label class="col-form-label" for="message_email">Email</label><input class="form-control form-control-lg" type="text" name="message_email" value="<?php echo esc_attr($_POST['message_email']); ?>"></div>
